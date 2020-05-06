2cac335e39c0c626d852bd983a501a1b44d16329
Patrick Harl <therandomwoodworker@gmail.com>
2020-05-06 14:37:16 -0700

    project completed

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,30 @@
+import { useLocalStorage } from './useLocalStorage'
+import { useEffect } from 'react'
+
+
+export const useDarkMode = () => {
+
+
+    const [darkMode, setDarkMode] = useLocalStorage('darkMode')
+
+    useEffect(() => {
+
+
+        if(darkMode)
+        {
+
+            document.querySelector('body').classList.add('dark-mode')
+
+        }
+        else {
+
+            document.querySelector('body').classList.remove('dark-mode')
+
+        }
+
+
+    }, [darkMode])
+
+    return [darkMode, setDarkMode]
+
+}
